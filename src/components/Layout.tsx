cef39394196272f3aebfc275e2ca65938aa554ba
Andrew Chang-DeWitt <andrewchangdewitt@gmail.com>
2020-05-11 15:17:38 -0400

    adding page grid styles & structure

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,20 +2,25 @@ import React, { FunctionComponent } from 'react'
 
 import styles from './Layout.module.sass'
 
+import { Header } from './Header'
+
 export interface Props {
   pageTitle: string
 }
 
-export const Layout: FunctionComponent<Props> = ({ pageTitle, children }) => (
+export const Layout: FunctionComponent<Props> = ({
+  // pageTitle,
+  children,
+}) => (
   <div>
     <div className={`${styles.header} layout-header`}>
-      <h1>
-        Hello:{' '}
-        <span className={` ${styles.pageTitle} layout-pageTitle`}>
-          {pageTitle}
-        </span>
-      </h1>
+      <Header />
     </div>
-    <div className={`${styles.content} layout-content`}>{children}</div>
+    <div className={styles.pageGrid}>
+      <div className={styles.navbar}></div>
+      <div className={styles.page}>
+        <div className={`${styles.content} layout-content`}>{children}</div>
+      </div>
+    </div>
   </div>
 )
