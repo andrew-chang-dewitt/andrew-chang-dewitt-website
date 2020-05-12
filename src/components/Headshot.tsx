6480f04e3baf377207493bd5ce51389e9d933165
Andrew Chang-DeWitt <andrewchangdewitt@gmail.com>
2020-05-12 13:35:42 -0400

    adding headshot

---
+++ src/components/Headshot.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import styles from './Headshot.module.sass'
+
+export const Headshot = () => (
+  <div className={styles.container}>
+    <img src="headshot.jpg" />
+  </div>
+)
