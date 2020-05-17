1648728f7dd6690bebcb136db0ff7ec0eb9f3748
Andrew Chang-DeWitt <andrewchangdewitt@gmail.com>
2020-05-17 09:09:41 -0400

    fixing swp error

--- src/components/pages/Landing.tsx
+++ src/components/pages/Landing.tsx
@@ -8,22 +8,22 @@ export const Landing = () => (
     <h1 className="title">
       Andrew <span className="no-wrap">Chang-DeWitt</span>
     </h1>
-      <div className={styles.nav}>
-        <span className={styles.navOption}>
-          <Link to="/hire-me">Hire Me</Link>
-        </span>{' '}
-        |{' '}
-        <span className={styles.navOption}>
-          <Link to="/blog">Blog</Link>
-        </span>{' '}
-        |{' '}
-        <span className={styles.navOption}>
-          <Link to="/about">About</Link>
-        </span>{' '}
-        |{' '}
-        <span className={styles.navOption}>
-          <Link to="/contact">Contact</Link>
-        </span>
-      </div>
+    <div className={styles.nav}>
+      <span className={styles.navOption}>
+        <Link to="/hire-me">Hire Me</Link>
+      </span>{' '}
+      |{' '}
+      <span className={styles.navOption}>
+        <Link to="/blog">Blog</Link>
+      </span>{' '}
+      |{' '}
+      <span className={styles.navOption}>
+        <Link to="/about">About</Link>
+      </span>{' '}
+      |{' '}
+      <span className={styles.navOption}>
+        <Link to="/contact">Contact</Link>
+      </span>
+    </div>
   </div>
 )
