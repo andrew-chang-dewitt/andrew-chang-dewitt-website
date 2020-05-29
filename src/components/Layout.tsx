3516af5f616573a43c6c889e4d9ffe1972098693
Andrew Chang-DeWitt <andrewchangdewitt@gmail.com>
2020-05-29 13:50:41 -0400

    stripping previous components

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,30 +2,26 @@ import React, { FunctionComponent } from 'react'
 
 import styles from './Layout.module.sass'
 
-import { Header } from './Header'
-import { NavPanel } from './NavPanel'
+// export interface Props {
+//   navPanelStyle: string
+// }
 
-export interface Props {
-  navPanelStyle: string
-}
-
-export const Layout: FunctionComponent<Props> = ({
-  navPanelStyle,
+// export const Layout: FunctionComponent<Props> = ({
+export const Layout: FunctionComponent = ({
+  // navPanelStyle,
   children,
 }) => (
   <div>
-    <div className={styles.header}>
-      <Header />
-    </div>
+    <div className={styles.header}>{/* <Header /> */}</div>
     <div className={styles.pageGrid}>
       <div className={`${styles.navbar}`}>
-        <NavPanel navPanelStyle={navPanelStyle} />
+        {/* <NavPanel navPanelStyle={navPanelStyle} /> */}
       </div>
       <div className={styles.content}>{children}</div>
     </div>
   </div>
 )
 
-Layout.defaultProps = {
-  navPanelStyle: 'default',
-}
+// Layout.defaultProps = {
+//   navPanelStyle: 'default',
+// }
