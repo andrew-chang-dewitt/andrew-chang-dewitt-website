8ddc9223da678fac33ed857f422bf52019a4f403
Andrew Chang-DeWitt <andrewchangdewitt@gmail.com>
2020-06-22 12:30:03 -0400

    header, contains navigation

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import styles from './Header.module.sass'
 import { NavMenu } from '../navigation/NavMenu'
@@ -8,13 +9,19 @@ import { NavTab } from '../navigation/NavTab'
 
 export const Header = () => (
   <header className={styles.header}>
-    <div className={styles.brandingArea}></div>
-    <NavMenu>
-      <NavTab destination="/#about">About</NavTab>
-      <NavTab destination="/#projects">Projects</NavTab>
-      <NavTab destination="/#hire-me">Hire Me</NavTab>
-      <NavTab destination="/#blog">Blog</NavTab>
-      <NavTab destination="/#contact">Contact Me</NavTab>
-    </NavMenu>
+    <div className={`${styles.contents} standardWidth`}>
+      <div className={styles.branding}>
+        <Link to="/#home">ACD</Link>
+      </div>
+      <div className={styles.menu}>
+        <NavMenu>
+          <NavTab destination="/#about">About</NavTab>
+          <NavTab destination="/#featured-projects">Projects</NavTab>
+          <NavTab destination="/#hire-me">Hire Me</NavTab>
+          <NavTab destination="/#blog-preview">Blog</NavTab>
+          <NavTab destination="/#contact">Contact Me</NavTab>
+        </NavMenu>
+      </div>
+    </div>
   </header>
 )
