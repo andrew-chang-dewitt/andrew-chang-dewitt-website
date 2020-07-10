ab644bde81002d36dea39f1e50debdb975d8fc83
Andrew Chang-DeWitt <adoit90@gmail.com>
2020-07-10 16:30:56 -0400

    pushing comments discussing possible fix to focus + smooth scroll bug

--- src/components/navigation/AnchorLink.tsx
+++ src/components/navigation/AnchorLink.tsx
@@ -1,6 +1,8 @@
 import React, { FunctionComponent, MutableRefObject } from 'react'
 import Link from 'gatsby-link'
 
+import { useLocation } from '@reach/router'
+
 interface Props {
   to: string
   target: MutableRefObject<any>
@@ -17,11 +19,20 @@ export const AnchorLink: FunctionComponent<Props> = ({
   id,
   className,
 }) => {
+  const location = useLocation()
   // side effects document.activeElement by calling focus on a given Ref
   // FIXME: currently the call to .focus() is happening before
   // css's `scroll-behavior: smooth` has a chance to actually scroll to
   // the target
+  // >>> POSSIBLE FIX:
+  // try having target not be the ref, but instead a function that'll 
+  // modify a target Component's `takeFocus` prop, 
+  // then, in the target Component, have a useEffect that watches for
+  // changes to takeFocus & that the component is in the current 
+  // viewport, then gives focus to the desired ref in the Component
+  // gonna take some refactoring though.
   const giveFocus = (_: React.MouseEvent): void => {
+    console.log(location)
     target.current.focus()
   }
 
