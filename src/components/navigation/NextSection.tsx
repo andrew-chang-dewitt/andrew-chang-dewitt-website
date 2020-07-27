e2a6915f9bd1e8707ff94838a0f34e487d5223a6
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2020-07-27 14:24:13 -0400

    next section icon link styled & functional

--- src/components/navigation/NextSection.tsx
+++ src/components/navigation/NextSection.tsx
@@ -2,6 +2,8 @@ import React, { RefObject } from 'react'
 
 import { AnchorLink } from './AnchorLink'
 
+import styles from './NextSection.module.sass'
+
 interface Props {
   to: string
   id: string
@@ -10,7 +12,7 @@ interface Props {
 
 export const NextSection = ({ to, target, id }: Props) => (
   <AnchorLink to={to} id={id} target={target}>
-    <svg>
+    <svg className={styles.svgIcon} width={100} height={100}>
       <title>Go to next section</title>
       <use xlinkHref="ikonate.svg#chevron-down" />
     </svg>
