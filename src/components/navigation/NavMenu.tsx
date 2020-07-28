00c1c035ba6c647a94c07aba73e2b320282e5c20
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2020-07-28 14:21:39 -0400

    refactored hamburger icon to own component + changed to managing menu status w/ javascript to allow it to also trigger svg animation + switched to list for menu semantics

--- src/components/navigation/NavMenu.tsx
+++ src/components/navigation/NavMenu.tsx
@@ -1,6 +1,7 @@
 import React, { RefObject } from 'react'
 
 import { NavTab } from './NavTab'
+import { HamburgerIcon } from './HamburgerIcon'
 
 import styles from './NavMenu.module.sass'
 
@@ -17,33 +18,18 @@ interface Props {
 }
 
 export const NavMenu = ({ items }: Props) => {
+  const [menuOpen, setMenuOpen] = React.useState(false)
+
+  const toggleMenu = (): void => {
+    setMenuOpen(!menuOpen)
+  }
+
   return (
     <nav className={styles.navigation}>
-      <input
-        className={styles.hamburgerInput}
-        id="button"
-        type="checkbox"
-        aria-label="Main Menu"
-      />
-      <label className={styles.hamburgerLabel} htmlFor="button">
-        <svg
-          className={styles.svgIcon}
-          width={60}
-          height={60}
-          viewBox="0 0 100 100"
-        >
-          <path
-            className={`${styles.line} ${styles.line1}`}
-            d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
-          />
-          <path className={`${styles.line} ${styles.line2}`} d="M 20,50 H 80" />
-          <path
-            className={`${styles.line} ${styles.line3}`}
-            d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
-          />
-        </svg>
-      </label>
-      <div className={styles.menu}>
+      <div className={styles.hamburgerButton}>
+        <HamburgerIcon opened={menuOpen} buttonHandler={toggleMenu} />
+      </div>
+      <ul className={`${styles.menu} ${menuOpen ? styles.opened : ''}`}>
         {items.map(({ key, to, text, active, targetRef }) => (
           <NavTab
             key={key}
@@ -54,7 +40,7 @@ export const NavMenu = ({ items }: Props) => {
             contentTarget={targetRef}
           />
         ))}
-      </div>
+      </ul>
     </nav>
   )
 }
