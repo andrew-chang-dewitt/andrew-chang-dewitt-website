ebb992501f9e1558748c773de3f07ed9f16b44ed
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2020-08-02 10:42:39 -0400

    switching to Contact module

---
+++ src/components/pages/contact/Contact.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+import styles from './Contact.module.sass'
+
+export const Contact = () => (
+  <div>
+    <h2 className={`${styles.firstSubtitle} subtitle`}>reach out</h2>
+    <p>
+      Questions about me or my work, reading suggestions, or you'd like to talk
+      about what I could do for you? I'd love to hear from you! Send me a
+      message using the form or any of the social media platforms below, or just
+      send me an email at{' '}
+      <a href="mailto:hello@andrew-chang-dewitt.dev">
+        hello@andrew-chang-dewitt.dev
+      </a>
+    </p>
+    <div className={styles.iconGrid}>
+      <div className={styles.icon}>
+        <a
+          href="https://github.com/andrew-chang-dewitt"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <svg>
+            <title>GitHub</title>
+            <use xlinkHref="icons/icons8/sprite.min.svg#icon-github"></use>
+          </svg>
+        </a>
+      </div>
+      <div className={styles.icon}>
+        <a
+          href="https://stackoverflow.com/users/4642869/andrew-chang-dewitt"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <svg>
+            <title>Stack Overflow</title>
+            <use xlinkHref="icons/icons8/sprite.min.svg#icon-stackoverflow"></use>
+          </svg>
+        </a>
+      </div>
+      <div className={styles.icon}>
+        <a
+          href="https://www.linkedin.com/in/adwtt90/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <svg>
+            <title>LinkedIn</title>
+            <use xlinkHref="icons/icons8/sprite.min.svg#icon-linkedin"></use>
+          </svg>
+        </a>
+      </div>
+      <div className={styles.icon}>
+        <a
+          href="https://twitter.com/a_chang_dewitt"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <svg>
+            <title>Twitter</title>
+            <use xlinkHref="icons/icons8/sprite.min.svg#icon-twitter"></use>
+          </svg>
+        </a>
+      </div>
+    </div>
+  </div>
+)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import React, { useRef } from 'react'
 import { Layout, navItems, mergeRefsToItems } from '../components/Layout'
 import { Section } from '../components/Section'
 import { Story } from '../components/pages/story/Story'
+import { Contact } from '../components/pages/contact/Contact'
 
 export default function Landing() {
   const storyRef = useRef<HTMLDivElement>(null)
@@ -38,30 +39,7 @@ export default function Landing() {
         */}
 
       <Section id="contact" title="Contact" ref={contactRef}>
-        <p>
-          Dignissim enim sit amet venenatis. Urna neque viverra justo nec
-          ultrices dui. Id aliquet risus feugiat in ante metus dictum. Proin
-          libero nunc consequat interdum varius. Pretium viverra suspendisse
-          potenti nullam ac. Odio eu feugiat pretium nibh ipsum consequat.
-          Quisque non tellus orci ac auctor augue mauris augue neque. Eu
-          ultrices vitae auctor eu augue ut lectus arcu bibendum. Aliquam ut
-          porttitor leo a diam. Tortor pretium viverra suspendisse potenti
-          nullam ac tortor vitae. Turpis in eu mi bibendum neque egestas congue
-          quisque. Ornare massa eget egestas purus viverra accumsan in nisl
-          nisi. Diam sollicitudin tempor id eu nisl. Tellus integer feugiat
-          scelerisque varius morbi enim. Mauris pharetra et ultrices neque
-          ornare.
-          <br />
-          Hendrerit dolor magna eget est lorem ipsum dolor sit. Ultrices in
-          iaculis nunc sed. Urna cursus eget nunc scelerisque. Id interdum velit
-          laoreet id. Est pellentesque elit ullamcorper dignissim cras tincidunt
-          lobortis feugiat vivamus. Consequat ac felis donec et odio
-          pellentesque diam volutpat commodo. Nullam ac tortor vitae purus
-          faucibus ornare suspendisse sed. Lacus vestibulum sed arcu non odio
-          euismod lacinia. Scelerisque eleifend donec pretium vulputate sapien
-          nec. Aliquam faucibus purus in massa tempor nec feugiat nisl. Dolor
-          sed viverra ipsum nunc aliquet.
-        </p>
+        <Contact />
       </Section>
     </Layout>
   )
