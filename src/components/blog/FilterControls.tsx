0646bf34bbab207f4f28121b9746022e67da36a8
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2020-08-30 10:15:07 -0400

    tags hidden until asked for

--- src/components/blog/FilterControls.tsx
+++ src/components/blog/FilterControls.tsx
@@ -15,7 +15,7 @@ interface Props {
 
 const spacer = <span className={styles.spacer}>|</span>
 
-const useFilterChoices = () => {
+const useFilterChoices = (tagHandler: () => void) => {
   const {
     value: sortDirection,
     update: setSortDirection,
@@ -29,7 +29,7 @@ const useFilterChoices = () => {
 
   return (
     <div className={styles.filterChoices}>
-      <button className={styles.tab}>
+      <button className={styles.tab} onClick={tagHandler}>
         <svg className={styles.filterIcon}>
           <title>Filter by tag</title>
           <use xlinkHref="/icons/ikonate.svg#hash" />
@@ -56,17 +56,21 @@ const useFilterChoices = () => {
 
 export const FilterControls = ({ tags, currentTag }: Props) => {
   const [controlsOpened, setControlsOpened] = React.useState(false)
+  const [tagsOpened, setTagsOpened] = React.useState(false)
 
-  const toggleFilters = () => {
+  const toggleControls = () => {
     setControlsOpened(!controlsOpened)
   }
+  const toggleTagFilters = () => {
+    setTagsOpened(!tagsOpened)
+  }
 
-  const filterChoices = useFilterChoices()
+  const filterChoices = useFilterChoices(toggleTagFilters)
 
   return (
     <div>
       <div className={styles.tabContainer}>
-        <button className={styles.tab} onClick={toggleFilters}>
+        <button className={styles.tab} onClick={toggleControls}>
           <svg className={styles.filterIcon}>
             <title>Filter & sort posts</title>
             <use xlinkHref="/icons/ikonate.svg#controls-vertical-alt" />
@@ -81,35 +85,37 @@ export const FilterControls = ({ tags, currentTag }: Props) => {
           </span>
         ) : null}
       </div>
-      <ul className={styles.tags}>
-        {tags.map((tag: string, current: number) => {
-          if (current === tags.length - 1)
-            return (
-              <li
-                key={current.toString()}
-                className={currentTag === tag ? styles.currentTag : ''}
-              >
-                <Link to={`/blog/tags/${kebabCase(tag)}`}>{tag}</Link>
-              </li>
-            )
-          else
-            return (
-              <li
-                key={current.toString()}
-                className={currentTag === tag ? styles.currentTag : ''}
-              >
-                <Link to={`/blog/tags/${kebabCase(tag)}`}>{tag}</Link>
-                {spacer}
-              </li>
-            )
-        })}
-        {currentTag ? (
-          <li>
-            {spacer}
-            <Link to="/blog">all posts</Link>
-          </li>
-        ) : null}
-      </ul>
+      {tagsOpened ? (
+        <ul className={styles.tags}>
+          {tags.map((tag: string, current: number) => {
+            if (current === tags.length - 1)
+              return (
+                <li
+                  key={current.toString()}
+                  className={currentTag === tag ? styles.currentTag : ''}
+                >
+                  <Link to={`/blog/tags/${kebabCase(tag)}`}>{tag}</Link>
+                </li>
+              )
+            else
+              return (
+                <li
+                  key={current.toString()}
+                  className={currentTag === tag ? styles.currentTag : ''}
+                >
+                  <Link to={`/blog/tags/${kebabCase(tag)}`}>{tag}</Link>
+                  {spacer}
+                </li>
+              )
+          })}
+          {currentTag ? (
+            <li>
+              {spacer}
+              <Link to="/blog">all posts</Link>
+            </li>
+          ) : null}
+        </ul>
+      ) : null}
     </div>
   )
 }
