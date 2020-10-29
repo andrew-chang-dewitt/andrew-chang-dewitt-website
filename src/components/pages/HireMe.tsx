e7f32c925ca114746cad2f0ad5ad520b6f1c2197
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2020-10-29 10:17:19 -0400

    Add Hire Me section components
    Creates the component to render a Hire Me section to the homepage. Not
    yet hooked up.

---
+++ src/components/pages/HireMe.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import styles from './HireMe.module.sass'
+
+export const HireMe = () => (
+  <div className="indent">
+    <h2 className={`subtitle ${styles.firstSubtitle}`}>why me</h2>
+
+    <p>
+      I have experience in web development, IOT microservices, & CLI utilities
+      all built with a variety of tools, including Javascript, Typescript,
+      Python, React, & others. As a self-taught developer, I have a proven track
+      record as an independent & fast learner capable of breaking down large,
+      complex problems into easily manageable pieces. Additionally, I have
+      extensive practice at identifying when, & how, to seek assistance.
+    </p>
+
+    <p>
+      Some <Link to="/#featured-projects">examples of my work</Link> can be
+      found above. Additionally, <Link to="/resume">my resume</Link> is
+      available on this site, along with <Link to="/blog">my blog</Link>,
+      containing posts on{' '}
+      <Link to="/blog/tags/problem-solution">problem solving</Link>,{' '}
+      <Link to="/blog/tags/react">React</Link>,{' '}
+      <Link to="/blog/tags/typescript">TypeScript</Link>, and more.
+    </p>
+
+    <h2 className="subtitle">why you</h2>
+
+    <p>
+      I'm looking for an employer with a commitment to mentorship & employee
+      development who's hiring entry-level programmers.
+    </p>
+  </div>
+)
