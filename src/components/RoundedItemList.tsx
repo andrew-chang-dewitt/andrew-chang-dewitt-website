58a7c70da1a6fd501e7087d22b47a8eab6d1ea8c
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2020-11-02 21:23:31 -0500

    Improve accessibility
    Make `accessibleName` required to improve accessibility of the rendered
    list

--- src/components/RoundedItemList.tsx
+++ src/components/RoundedItemList.tsx
@@ -4,10 +4,10 @@ import styles from './RoundedItemList.module.sass'
 
 interface Props {
   items: Array<string>
-  accessibleName?: string
+  accessibleName: string
 }
 
-export default ({ items, accessibleName = undefined }: Props) => (
+export default ({ items, accessibleName }: Props) => (
   <ul className={styles.list} aria-label={accessibleName}>
     {items.map((item) => (
       <li key={item}>{item}</li>
