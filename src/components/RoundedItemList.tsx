8cb78cff26a2e196683b609e541f202664375f4a
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2020-11-03 00:11:57 -0500

    Revert "Revert "Merge pull request #56 from andrew-chang-dewitt/master""
    This reverts commit b7fe3ac5b8b120c9468acf29b2db490ea70e9841.

---
+++ src/components/RoundedItemList.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import styles from './RoundedItemList.module.sass'
+
+interface Props {
+  items: Array<string>
+  accessibleName: string
+}
+
+export default ({ items, accessibleName }: Props) => (
+  <ul className={styles.list} aria-label={accessibleName}>
+    {items.map((item) => (
+      <li key={item}>{item}</li>
+    ))}
+  </ul>
+)
