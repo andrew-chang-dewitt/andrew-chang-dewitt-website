e8db46eb45c177076aa566dcdd8f3641df092453
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2021-04-26 16:32:41 -0400

    Load global variables needed to test Gatsby's Link.

--- test-setup.js
+++ test-setup.js
@@ -11,6 +11,7 @@ function copyProps(src, target) {
   })
 }
 
+// setup jsodom
 global.window = window
 global.document = window.document
 global.navigator = {
@@ -23,3 +24,15 @@ global.cancelAnimationFrame = function (id) {
   clearTimeout(id)
 }
 copyProps(window, global)
+
+// mock out global properties that Gatsby's Link requires to not throw errors
+Object.defineProperty(global, '__BASE_PATH__', {
+  value: '',
+  writable: true,
+})
+Object.defineProperty(global, '___loader', {
+  value: {
+    enqueue: () => {},
+  },
+  writable: true,
+})
