8dd8d9698abba8ab0615d1760fd4ad71f2394e3f
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2021-10-26 15:48:57 -0400

    Fix missing field in data query.

--- gatsby-node.js
+++ gatsby-node.js
@@ -93,6 +93,7 @@ exports.onPostBuild = async ({ graphql, reporter }) => {
             education {
               school
               degree
+              major
               minor
               location
               date
